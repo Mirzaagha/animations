6ad52ef79a08f4919eb935d4412ef91f918462f9
Mirzaagha Guliyev <35248449+Mirzaagha@users.noreply.github.com>
2024-01-27 22:06:51 +0100

    Update spinOfMatrixOfSquares.js

--- spinOfMatrixOfSquares.js
+++ spinOfMatrixOfSquares.js
@@ -1,22 +1,10 @@
 function setup() {
   createCanvas(400, 400);
-  // background(220);
-  // for(var x = 2*L; x < cr*L+2*L;x+=L){
-  //   for(var y = 2*L; y < cr*L+2*L;y+=L){
-  //     var xx = (x-2*L)/L;
-  //     var yy = (y-2*L)/L;
-  //     var zz = (cr/2 - xx)*(cr/2 - xx)+(cr/2 - yy)*(cr/2 - yy);
-  //     r_a(x,y,PI/(3+zz),L);
-  //     print(xx,yy,zz);
-  //   }
-  // }
 }
-// cr/2-x;
 function r_a(x1,y1,a,size,st){
   for(var x = 0;x < 4;x++){
     line(x1,y1,x1+size*sin(a+(x*PI)/2),y1+size*cos(a+(x*PI)/2));
     strokeWeight((st+2)*1);
-    // map((st+1),0,2,0,255);
     stroke(map((st+1),0,2,0,255),0,map((st+1),0,2,255,0));
     x1 = x1+size*sin(a+(x*PI)/2);
     y1 = y1+size*cos(a+(x*PI)/2);
@@ -36,12 +24,10 @@ function draw() {
       var yy = (y-2*L)/L;
       var zz = (cr/2 - xx)*(cr/2 - xx)+(cr/2 - yy)*(cr/2 - yy);
       r_a(x,y,(PI/(3+zz))+z,L,z);
-      // print(xx,yy,zz);
     }
   }
   ch += 1;
   z = 1*sin(ch*PI/180);
-  // print(z);
 }
 
 
